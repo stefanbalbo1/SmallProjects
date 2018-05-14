00616cc4402af7ac3bf1a72e6b2613dd6ef06a1b
stefanbalbo1 <sbalbo@gmail.com>
2018-05-14 12:07:17 -0700

    Output Release

--- ReleaseCalculator/ReleaseCalculator/ReleaseCalculator.h
+++ ReleaseCalculator/ReleaseCalculator/ReleaseCalculator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <ctime>
+#include <fstream>
 
 class ReleaseCalculator
 {
@@ -8,9 +9,12 @@ public:
   bool CalculateRelease();
   int Date2DayNumber(const std::tm& date);
   std::tm DayNumber2Date(int dayNumber);
+  void AddDays(std::tm &date, int days);
 protected:
   bool EnterDate(std::tm & date);
   bool EnterIterations(int& nIterations);
   bool OutputRelease(const std::tm& startDate, int nIterations);
+  void OutputDate(std::ofstream& outputFile, std::tm date);
+  
 
 };
