fb6d64927f9844bb20c501ef7160b0ae90cdc1c3
stefanbalbo1 <sbalbo@gmail.com>
2020-02-14 10:51:45 -0800

    Added custom iteration leghts and additional release events

--- ReleaseCalculator/ReleaseCalculator/ReleaseCalculator.h
+++ ReleaseCalculator/ReleaseCalculator/ReleaseCalculator.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <ctime>
 #include <fstream>
+#include <vector>
 
 class ReleaseCalculator
 {
 public:
-  ReleaseCalculator() {};
+  ReleaseCalculator() : m_iterationLengths(48,4) {};
   bool CalculateRelease();
   int Date2DayNumber(const std::tm& date);
   std::tm DayNumber2Date(int dayNumber);
@@ -15,6 +16,6 @@ protected:
   bool EnterIterations(int& nIterations);
   bool OutputRelease(const std::tm& startDate, int nIterations);
   void OutputDate(std::ofstream& outputFile, std::tm date);
-  
+  std::vector<int> m_iterationLengths;
 
 };
